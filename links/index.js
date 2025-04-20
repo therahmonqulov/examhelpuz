eb9ddb761bba4e7f3386e3766f27ef9b05da5444
therahmonqulov <therahmonqulov@gmail.com>
2025-04-20 19:27:36 +0500

    add

--- links/index.js
+++ links/index.js
@@ -235,7 +235,7 @@ forme.addEventListener('submit', e => {
   // 1 soniya ichida o‘tkazamiz (brauzer fetchni orqa fonda bajaradi)
   setTimeout(() => {
       window.location.assign("./thank-you.html");
-  }, 1000);
+  }, 10);
 });
 
 
